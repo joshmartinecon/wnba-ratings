4813c5a26a004f9df33b3b60ad99e4a2255d0801
joshmartinecon <jmartin2013@gmail.com>
2025-07-31 18:28:34 -0400

    updated 7/31/25

--- webscrape and analysis.R
+++ webscrape and analysis.R
@@ -5,8 +5,122 @@ library(dplyr)
 ### clear data environment
 # rm(list=ls())
 
-### Negate funciton
+### Negate function
 '%ni%' <- Negate('%in%')
+
+### web scrape box score links
+teams <- c("atl", "chi", "conn", "dal", "gs", "ind", "lv", "la", "min", "ny", "phx", "sea", "wsh")
+y <- list()
+
+### loop
+for(i in teams){
+  
+  ### start
+  x <- read_html(paste0("https://www.espn.com/wnba/team/schedule/_/name/", i))
+  
+  x %>% 
+    html_table() %>%
+    as.data.frame() -> z
+  colnames(z) <- z[2,]
+  z <- z[c(-1:-2),]
+  z <- z[1:((1:nrow(z))[grepl("Regular|Cup", z[,1])]-1),]
+  
+  ### links
+  x %>%
+    html_elements("a") %>%
+    html_attr("href") -> a
+  
+  ### game links
+  b <- unique(a[grepl("gameId", a)])
+  b <- b[!grepl("preview", b)]
+  
+  ### box score links
+  a <- gsub("game/_/", "boxscore/_/", b)
+  a <- sub("/[^/]*$", "", a)
+  
+  y[[length(y)+1]] <- a[1:nrow(z)]
+  
+  Sys.sleep(5)
+}
+
+### game scrape
+a <- unique(unlist(y))
+b <- list()
+
+for(i in 1:length(a)){
+  
+  ### start
+  y <- read_html(a[i]) %>% 
+    html_table()
+  
+  ### team 1
+  x <- cbind(as.data.frame(y[2]), as.data.frame(y[3]))
+  colnames(x) <- x[1,]
+  x <- x[-1,]
+  x <- data.frame(
+    player = sub("([A-Z][a-z]+ [A-Z][a-z]+).*", "\\1", x[,1]),
+    mp = ifelse(x[,2] == "DNP-COACH'S DECISION", 0, x[,2]),
+    team = as.data.frame(y[1])[1,1]
+  )
+  x <- x[!grepl("bench|team", x$player) & x$player != "" & !grepl("DNP", x$mp),]
+  
+  ### team 2
+  z <- cbind(as.data.frame(y[4]), as.data.frame(y[5]))
+  colnames(z) <- z[1,]
+  z <- z[-1,]
+  z <- data.frame(
+    player = sub("([A-Z][a-z]+ [A-Z][a-z]+).*", "\\1", z[,1]),
+    mp = ifelse(z[,2] == "DNP-COACH'S DECISION", 0, z[,2]),
+    team = as.data.frame(y[1])[2,1]
+  )
+  z <- z[!grepl("bench|team", z$player) & z$player != "" & !grepl("DNP", z$mp),]
+  
+  ### combine
+  b[[length(b)+1]] <- rbind(x, z)
+  Sys.sleep(5)
+  cat(round(i/length(a), 2), "\r")
+}
+
+### compile and clean data
+y <- as.data.frame(do.call(rbind, b))
+
+z <- as.data.frame(table(paste(y$player, y$team)))
+x <- aggregate(as.numeric(mp) ~ player + team, y, sum)
+x <- data.frame(
+  player = x[,1],
+  team = x[,2],
+  g = z$Freq[match(paste(x$player, x$team), z$Var1)],
+  mp = x[,3]
+)
+x$mp_g <- round(x$mp / x$g, 2)
+x[x$player == "JJ QuinerlyJ. Quinerly#11",1] <- "JJ Quinerly"
+x[x$player == "Teaira Mc",1] <- "Teaira McCowan"
+x[x$player == "Aari Mc",1] <- "Aari McDonald"
+x[x$player == "A'ja WilsonA. Wilson#22",1] <- "A'ja Wilson"
+x[x$player == "Kayla Mc",1] <- "Kayla McBride"
+x[x$player == "Megan Mc",1] <- "Megan McConnell"
+x$player[x$player == "Monique Akoa"] <- "Monique Akoa Makani"
+x$player[x$player == "Sarah Ashlee"] <- "Sarah Ashlee Barker"
+x$player[x$player == "Myisha Hines"] <- "Myisha Hines-Allen"
+x$player[x$player == "Leila Lacan"] <- "Leïla Lacan"
+x$player[x$player == "Olivia Nelson"] <- "Olivia Nelson-Ododa"
+x$player[x$player == "Olivia Nelson"] <- "Olivia Nelson-Ododa"
+x$player[x$player == "Te-Hina Paopao"] <- "Te-Hina PaoPao"
+x$player[x$player == "Janelle Salaun"] <- "Janelle Salaün"
+x$player[x$player == "Hailey Van"] <- "Hailey Van Lith"
+x$player[x$player == "Shatori Walker"] <- "Shatori Walker-Kimbrough"
+
+x$team <- ifelse(x$team == "CONN", "CON",
+                 ifelse(x$team == "GS", "GSV",
+                        ifelse(x$team == "LA", "LAS",
+                               ifelse(x$team == "LV", "LVA",
+                                      ifelse(x$team == "NY", "NYL",
+                                             ifelse(x$team == "PHX", "PHO",
+                                                    ifelse(x$team == "WSH", "WAS", x$team)))))))
+
+### save 
+setwd("C:/Users/jmart/OneDrive/Desktop/GitHub/wnba-ratings")
+# write.csv(x, "minutes played.csv", row.names = F)
 
 ##### player data and analysis #####
 
@@ -69,12 +183,17 @@ y <- y[paste(y$link, y$team) %in% paste(z$id, z$team),]
 
 ## remove players with little data
 y <- subset(y, as.numeric(y$mp) >= 40)
+y[,4:8] <- lapply(y[,4:8], as.numeric)
+
+## supplement minutes played measure
+x <- read.csv("minutes played.csv")
+y$mp_g <- x$mp_g[match(paste(y$player, y$team),
+                       paste(x$player, x$team))]
 
 ### create measure of minutes played per game, scale by team
-y[,4:8] <- lapply(y[,4:8], as.numeric)
-z <- aggregate(mp / g ~ team, y, sum)
+z <- aggregate(mp_g ~ team, y, sum)
 z$adj <- z[,2] / (40*5)
-y$mp_g <- y$mp / y$g / z$adj[match(y$team, z$team)]
+y$mp_g_team <- y$mp_g / z$adj[match(y$team, z$team)]
 
 ### standardize playing-time weighted efficiency variables
 for(i in 6:8){
@@ -83,31 +202,11 @@ for(i in 6:8){
 }
 y$score <- rowMeans(y[,6:8])
 
-### cor(playing time, rating) seems non-linear
-y$mp2 <- y$mp_g*y$mp_g
-y <- y[order(y$mp_g),]
-
-### store regression coefs for later
-lm1 <- lm(y$score ~ y$mp_g + y$mp2)
-a <- coef(lm1)[1]
-b <- coef(lm1)[2]
-c <- coef(lm1)[3]
-
-### predicted rating, based on playing time
-y$pred <- predict(lm1)
-
-### difference between actual score and prediction
-y$diff <- y$score - y$pred
-
 ### video-game style rating (logged)
-y$rating <- round(60 + (asinh(y$score) - min(asinh(y$score)))/
-                    (max(asinh(y$score)) - min(asinh(y$score))) * (100-60), 1)
+y$rating <- 60 + (asinh(y$score) - min(asinh(y$score)))/
+                    (max(asinh(y$score)) - min(asinh(y$score))) * (100-60)
 
-## visalize expected score and playing time
-plot(y$mp_g, y$score)
-lines(y$mp_g, y$pred, lwd = 4, lty = 2)
-
-### webscrape playing time
+### web scrape injuries
 w <- read_html("https://www.espn.com/wnba/injuries") %>%
   html_elements("tbody tr") %>%
   lapply(function(row) {
@@ -124,131 +223,94 @@ w <- read_html("https://www.espn.com/wnba/injuries") %>%
   bind_rows()
 w <- w[w$Status == "Out",]
 
-### assign playing time based on productivity
-teamz <- unique(y$team)
+### full squad playing time
+teams <- unique(y$team)
 x <- list()
-for(i in teamz){
-  
-  # filter by team
+for(i in teams){
   z <- y[y$team == i,]
-  
-  # remove injured players
+  cdf_fn <- ecdf(z$rating)
+  z$mp_g_star <- cdf_fn(z$rating)*max(y$mp_g)
+  z$mp_g_star <- z$mp_g_star / sum(z$mp_g_star) * 200
+  rule <- z$mp_g_star > max(y$mp_g)
+  if(TRUE %in% rule){
+    allocate_mins <- sum(z$mp_g_star[rule] - max(y$mp_g))
+    z$mp_g_star <- ifelse(!rule, z$mp_g_star + cdf_fn(z$rating) / sum(cdf_fn(z$rating)) * allocate_mins, max(y$mp_g))
+    z$mp_g_star <- z$mp_g_star / sum(z$mp_g_star) * 200
+  }
+  x[[length(x)+1]] <- z
+}
+y <- as.data.frame(do.call(rbind, x))
+
+### current squad playing time
+teams <- unique(y$team)
+x <- list()
+for(i in teams){
+  z <- y[y$team == i,]
   z <- z[z$player %ni% w$Name,]
-  
-  # expected minutes
-  minz <- (-1*b + sqrt(b^2 - 4*c*(a - z$score))) / (2*c)
-  
-  # lower and upper bounds on minutes
-  minz <- ifelse(is.na(minz), min(y$mp_g), minz)
-  minz <- pmin(pmax(minz, min(y$mp_g)), max(y$mp_g))
-  
-  # sums to 200 (5 players, 40 mins)
-  z$mp_g_star <- minz / sum(minz) * 200
-  
-  # can't play more than some amount
-  rulez <- z$mp_g_star > max(y$mp_g)
-  
-  # reallocate playing time if rule-breaker
-  while(TRUE %in% rulez){
-    xtra <- sum(z$mp_g_star[rulez] - max(y$mp_g))
-    
-    z$mp_g_star <- ifelse(rulez, max(y$mp_g), z$mp_g_star)
-    
-    z$mp_g_star[!rulez] <- z$mp_g_star[!rulez] + 
-      z$mp_g_star[!rulez] / sum(z$mp_g_star[!rulez]) * xtra
-    
-    rulez <- round(z$mp_g_star, 2) > max(round(y$mp_g, 2))
+  z$mp_g_team <- z$mp_g / sum(z$mp_g) * 200 
+  cdf_fn <- ecdf(z$rating)
+  z$mp_g_star <- cdf_fn(z$rating)*max(y$mp_g)
+  z$mp_g_star <- z$mp_g_star / sum(z$mp_g_star) * 200
+  rule <- z$mp_g_star > max(y$mp_g)
+  if(TRUE %in% rule){
+    allocate_mins <- sum(z$mp_g_star[rule] - max(y$mp_g))
+    z$mp_g_star <- ifelse(!rule, z$mp_g_star + cdf_fn(z$rating) / sum(cdf_fn(z$rating)) * allocate_mins, max(y$mp_g))
+    z$mp_g_star <- z$mp_g_star / sum(z$mp_g_star) * 200
   }
-  
-  # store team
-  z$all <- 0
   x[[length(x)+1]] <- z
-  
-  ### include injured players for full team stength calculation
-  # filter by team
-  z <- y[y$team == i,]
-  
-  # expected minutes
-  minz <- (-1*b + sqrt(b^2 - 4*c*(a - z$score))) / (2*c)
-  
-  # lower and upper bounds on minutes
-  minz <- ifelse(is.na(minz), min(y$mp_g), minz)
-  minz <- pmin(pmax(minz, min(y$mp_g)), max(y$mp_g))
-  
-  # sums to 200 (5 players, 40 mins)
-  z$mp_g_star <- minz / sum(minz) * 200
-  
-  # can't play more than some amount
-  rulez <- z$mp_g_star > max(y$mp_g)
-  
-  # reallocate playing time if rule-breaker
-  while(TRUE %in% rulez){
-    xtra <- sum(z$mp_g_star[rulez] - max(y$mp_g))
-    
-    z$mp_g_star <- ifelse(rulez, max(y$mp_g), z$mp_g_star)
-    
-    z$mp_g_star[!rulez] <- z$mp_g_star[!rulez] + 
-      z$mp_g_star[!rulez] / sum(z$mp_g_star[!rulez]) * xtra
-    
-    rulez <- round(z$mp_g_star, 2) > max(round(y$mp_g, 2))
-  }
-  
-  # store team
-  z$all <- 1
-  x[[length(x)+1]] <- z
-  
 }
 x <- as.data.frame(do.call(rbind, x))
-y1 <- x[x$all == 1,]
-x <- x[x$all == 0, 1:15]
 
-## difference between optimal and current playing time
-x$min_diff <- x$mp_g_star - x$mp_g
-
-## throw injured players back into data
-z <- y[y$player %ni% x$player,]
-z$player <- paste0(z$player, "+")
-z$mp_g <- 0
-z$mp_g_star <- 0
-z$min_diff <- 0
-x <- rbind(x, z)
-
-### sort by score
-x <- x[order(-x$score),]
-row.names(x) <- 1:nrow(x)
-
-### simplify data
-y <- x[,c(1, 3, 9, 15:16, 14)]
-
-##### team data and analysis #####
-
-### playing-time weighted team rating
-z <- aggregate(cbind(rating*mp_g, mp_g) ~ team, y, sum)
-z$rating <- z[,2] / z[,3]
-
-## optimal playing-time weighted team rating
-z_star <- aggregate(cbind(rating*mp_g_star, mp_g_star) ~ team, y, sum)
-z_star$rating <- z_star[,2] / z_star[,3]
-
-## optimal playing-time weighted team rating
-z_full <- aggregate(cbind(rating*mp_g_star, mp_g_star) ~ team, y1, sum)
-z_full$rating <- z_full[,2] / z_full[,3]
-
-### simplify data
+### current team rating
+z <- as.data.frame(aggregate(rating*mp_g_team ~ team, x, sum))
 z <- data.frame(
-  team = z$team,
-  rating = round(z$rating, 1),
-  strength = round(z_full$rating, 1),
-  rotation_rating = round(z$rating - z_star$rating, 1)
+  team = z[,1],
+  current_rating = z[,2] / 200
 )
+r <- z
 
-## sory by rating
-z <- z[order(-z$rating),]
+### full team, best rotation rating
+z <- as.data.frame(aggregate(rating*mp_g_star ~ team, y, sum))
+z <- data.frame(
+  team = z[,1],
+  current_better_rating = z[,2] / 200
+)
+r$best_rating <- z[match(r$team, z$team),2]
+
+### full team, best rotation rating
+z <- as.data.frame(aggregate(rating*mp_g_team ~ team, y, sum))
+z <- data.frame(
+  team = z[,1],
+  current_better_rating = z[,2] / 200
+)
+r$rotation_rating <- z[match(r$team, z$team),2] - r$best_rating
+ecdf_fn <- ecdf(r$rotation_rating)
+r$rotation_rating <- ecdf_fn(r$rotation_rating)
+
+### simplify data
+r[,2:4] <- round(r[,2:4], 1)
+colnames(r)[2:3] <- c("rating", "strength")
+r <- r[order(-r$rating),]
+
+### add back in injured players
+z <- y[y$link %ni% x$link,]
+z$mp_g_team <- z$mp_g_star <- 0
+x <- rbind(x, z)
+x <- data.frame(
+  player = x$player,
+  team = x$team,
+  mp_g = round(x$mp_g_team),
+  mp_g_star = round(x$mp_g_star),
+  min_diff = 0,
+  rating = x$rating
+)
+x$min_diff <- x$mp_g_star - x$mp_g
+x <- x[order(-x$rating),]
+x$rating <- round(x$rating)
 
 ##### save work #####
 
 setwd("C:/Users/jmart/OneDrive/Desktop/GitHub/wnba-ratings")
-y[,3:6] <- apply(y[,3:6], 2, round)
-# write.csv(y, "WNBA_Ratings_and_Rotations.csv", row.names = F)
-# write.csv(z, "WNBA_Team_Ratings.csv", row.names = F)
+# write.csv(x, "WNBA_Ratings_and_Rotations.csv", row.names = F)
+# write.csv(r, "WNBA_Team_Ratings.csv", row.names = F)
 # write.csv(Sys.time(), "last_updated.csv")
